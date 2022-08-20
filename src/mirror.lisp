1c0f17db89fdc14404b27e56e257d57d8d851cd7
Joel Boehland <jboehland@gmail.com>
2022-08-20 16:23:22 -0700

    Fix y/h typo. Remove CLX specific semantics.

--- src/mirror.lisp
+++ src/mirror.lisp
@@ -11,22 +11,7 @@
     (sheet  (sdl2-drawable (sheet-mirror object)))
     (medium (sdl2-drawable (medium-drawable object)))
     (sdl2-mirror (window object))
-    ;; (xlib:drawable object)
     (null nil)))
-
-(defun %set-window-name (window name)
-  (setf (xlib:wm-name window) (%ensure-standard-characters name))
-  (xlib:change-property window
-                        :_NET_WM_NAME
-                        (babel:string-to-octets name :encoding :utf-8)
-                        :UTF8_STRING 8))
-
-(defun %set-window-icon-name (window icon-name)
-  (setf (xlib:wm-icon-name window) (%ensure-standard-characters icon-name))
-  (xlib:change-property window
-                        :_NET_WM_ICON_NAME
-                        (babel:string-to-octets icon-name :encoding :utf-8)
-                        :UTF8_STRING 8))
 
 (defmethod port-set-mirror-name
     ((port sdl2-basic-port) (sheet mirrored-sheet-mixin) name)
@@ -48,7 +33,7 @@
           (when (or (/= x1 ox1) (/= y1 oy1))
             (sdl2:set-window-position (round-coordinate x1)(round-coordinate y1)))
           (when (or (/= w ow) (/= h oh))
-            (sdl:set-window-size (round-coordinate w) (round-coordinate y)))))
+            (sdl:set-window-size (round-coordinate w) (round-coordinate h)))))
       (values x1 y1 x2 y2))))
 
 (defmethod destroy-mirror ((port sdl2-basic-port) (sheet mirrored-sheet-mixin))
@@ -75,10 +60,6 @@
       (sdl:hide-window window))))
 
 (defmethod port-shrink-sheet ((port sdl2-basic-port) (sheet mirrored-sheet-mixin))
-  ;; Not all x11 window-managers iconify windows (in particular many
-  ;; tiling window-managers). In those window managers, after the call
-  ;; of shrink-frame McClim think that the frame is iconified but it
-  ;; is not. -- admich 2021-08-21
   (when-let ((window (window (sheet-direct-mirror sheet))))
     (let ((window (window mirror)))
       (sdl:minimize-window window))))
