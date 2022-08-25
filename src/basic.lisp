4e60d35daebe02e8713af0a9f34c137e708c1d5a
Joel Boehland <jboehland@gmail.com>
2022-08-25 12:47:26 -0700

    Cleanup unused cruft and comment test code.

--- src/basic.lisp
+++ src/basic.lisp
@@ -38,7 +38,6 @@
                 (sdl2-ffi.functions:sdl-wait-event-timeout event timeout))))
     (= rc 1)))
 
-
 ;;;; Event handling
 
 ;; The function ~handle-sdl2-event~ is used to implement event handling. When
@@ -47,29 +46,6 @@
 (defgeneric handle-sdl2-event (event-type event))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-
-  (defun %extract-parameter-pairs (params)
-    (log:info "params: ~s " params)
-    (let ((parameter-pairs nil))
-      (do ((keyword params (if (cdr keyword)
-                               (cddr keyword)
-                               nil)))
-          ((null keyword))
-        (push (list (first keyword) (second keyword)) parameter-pairs))
-      (nreverse parameter-pairs)))
-
-  (defun unpack-event-params (event-var event-type params)
-    (mapcar (lambda (param)
-              (let ((keyword (first param))
-                    (binding (second param))
-                    (ref (or (cdr (assoc event-type sdl2::*event-type-to-accessor*))
-                             :user)))
-                (if (eql keyword :user-data)
-                    `(,binding (get-user-data (c-ref ,event-var sdl2-ffi:sdl-event ,ref :code)))
-                    (if (and (or (eql ref :text) (eql ref :edit)) (eql keyword :text))
-                        `(,binding (c-ref ,event-var sdl2-ffi:sdl-event ,ref ,keyword string))
-                        `(,binding (c-ref ,event-var sdl2-ffi:sdl-event ,ref ,keyword))))))
-            params))
 
   (defun %plist-keywords (plist)
     (loop :for (k nil) :on plist :by #'cddr :collect k))
@@ -94,20 +70,8 @@
     `(let ,(%expand-binding-pairs-from-plist binding-names bindings-plist-sym)
        ,@body-forms))
 
-
-  (comment
-    (defvar p-names-1 '(window x y w h))
-    (defvar p-names-and-values-1 '(:window (:window-name "win-1") :x 200 :y 400 :w 1080 :y 720))
-    (setf p-names-and-values-1 '(:window (:window-name "win-1") :x 200 :y 400 :w 1080 :y 720))
-    (%expand-binding-form-from-plist x (alx:make-gensym "bindings-plist-"))
-    (%expand-plist-to-let-bindings-context p-names-1 (alx:make-gensym "bindings-plist-") '((format t "w: ~a" w)))
-    (%extract-parameter-pairs p-names-and-values-1)
-    (%plist-keywords p-names-and-values-1)
-    )
-
   (defun %expand-bindings-context-for-core-event (sdl-event event-type params forms)
-    (let (;(parameter-pairs (%extract-parameter-pairs params))
-          (parameter-pairs (%make-keyword-binding-pairs params)))
+    (let ((parameter-pairs (%make-keyword-binding-pairs params)))
       (log:info "extracted parameter pairs: ~s" parameter-pairs)
       `(let (,@(sdl2::unpack-event-params sdl-event
                                           event-type
@@ -117,21 +81,7 @@
   (defun %expand-bindings-context-for-user-event (sdl-event event-type binding-names body-forms)
     (let ((user-data-sym (alx:make-gensym "user-data-")))
       `(let* ((,user-data-sym (sdl2::get-user-data (sdl2::c-ref ,sdl-event sdl2-ffi:sdl-event :user :code))))
-         ,(%expand-plist-to-let-bindings-context binding-names user-data-sym body-forms)
-         )))
-
-  )
-
-(comment
-
-  (%kw-pair-to-binding-form :A 24)
-  (%expand-bindings-context-for-core-event 'user-event :windowevennt
-                                           '(event window-id timestamp data1 data2) '((format t "Got window: ~a ~%" window)))
-
-  (%expand-bindings-context-for-user-event 'user-event :sdl-exit-port '(foo bar) '((format t "Got foo: ~a ~%" foo)))
-
-  (%extract-parameter-pairs '(:foo 22 :bar 24))
-  (%unpack-user-event-params '(:foo 22 :bar 24))
+         ,(%expand-plist-to-let-bindings-context binding-names user-data-sym body-forms))))
 
   )
 
@@ -143,24 +93,25 @@
   `(defmethod handle-sdl2-event ((event-type (eql ,event-type)) ,event)
      ,(%expand-bindings-context-for-user-event event event-type event-params handler-body)))
 
+;;SDL Requests
+(defun push-event (event &rest args)
+  (if args
+      (log:info "would call sdl2:push-user-event: ~s with user-data: ~s ~%" event args)
+      (log:info "would call sdl2:push-user-event ~s with no user-data ~%" event)))
+
 (defmacro define-sdl2-request (request-fn-name (&rest event-params) &body handler-body)
-  (let ((user-event-type (alx:make-keyword request-fn-name))
-        (event-sym (alx:make-gensym "event-"))
-        (sync-sym (alx:make-gensym "sync-")))
+  (let ((user-event-type (alx:make-keyword request-fn-name)))
   `(progn
      (sdl2:register-user-event-type ,user-event-type)
 
-     ;; (defun ,request-fn-name (&rest event-params &key (synchronize nil synchronize-p) &allow-other-keys)
      (defun ,request-fn-name (,@event-params &key (synchronize nil synchronize-p))
-       ;; (remf event-params :synchronize)
        (cond ((null synchronize) (apply #'push-event ,user-event-type event-params))
              ((numberp synchronize) (apply #'push-event ,user-event-type event-params))
              (t (apply #'push-event ,user-event-type event-params))))
 
      (define-sdl2-user-event-handler (event ,user-event-type) (,@event-params)
-       ,@handler-body)
-    )
-    ))
+       ,@handler-body))))
+
 (comment
 (define-sdl2-request change-window-size (window x y w h)
     (sdl2-ffi.functions:sdl-set-window-position window x y)
@@ -180,30 +131,3 @@
       (handle-sdl2-window-event event-key sheet timestamp data1 data2)))
   )
 
-;;SDL Requests
-(defun push-event (event &rest args)
-  (if args
-      (log:info "would call sdl2:push-user-event: ~s with user-data: ~s ~%" event args)
-      (log:info "would call sdl2:push-user-event ~s with no user-data ~%" event)))
-
-(progn
-  ;; Register the :sdl-exit-port event type with the SDL2 event type registry
-  (sdl2:register-user-event-type :sdl-exit-port)
-
-  (defun sdl-exit-port (&rest args &key (synchronize nil synchronize-p) &allow-other-keys)
-    (remf args :synchronize)
-    (cond ((null synchronize) (apply #'push-event :sdl-exit-port args))
-          ((numberp synchronize) (apply #'push-event :sdl-exit-port-sync-num args))
-          (t (apply #'push-event :sdl-exit-port-sync-1 args)))
-
-    )
-
-  )
-
-
-
-(comment
-  (sdl-exit-port :foo 1 :bar 2)
-  (sdl-exit-port :synchronize .5 :foo 1 :bar 2)
-  (sdl-exit-port)
-  )
